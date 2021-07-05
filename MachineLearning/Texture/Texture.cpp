518a1dd39f540b80f28988a010630b02f705c080
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-07-05 20:03:52 +0200

    Added text to Texture

--- MachineLearning/Texture/Texture.cpp
+++ MachineLearning/Texture/Texture.cpp
@@ -1,7 +1,7 @@
 #include "Texture.h"
 
 #include <SDL_image.h>
-#include <SDL_video.h>
+#include <SDL_ttf.h>
 #include <SDL.h>
 
 #include "../Renderer/Renderer.h"
@@ -15,6 +15,36 @@ Texture::Texture(const std::string& path)
 	SDL_Surface* pSurface{ IMG_Load(path.c_str()) };
 
 	Utils::Assert(pSurface != nullptr, SDL_GetError());
+
+	m_Width = pSurface->w;
+	m_Height = pSurface->h;
+
+	m_pTexture = SDL_CreateTextureFromSurface(Renderer::GetInstance()->GetSDLRenderer(), pSurface);
+
+	Utils::Assert(m_pTexture != nullptr, SDL_GetError());
+
+	SDL_FreeSurface(pSurface);
+}
+
+Texture::Texture(const std::string& path, const std::string& text, const int ptSize, const MathUtils::RGBColour& colour)
+	: m_pTexture{}
+	, m_Width{}
+	, m_Height{}
+{
+	TTF_Font* pFont{ TTF_OpenFont(path.c_str(), ptSize) };
+
+	Utils::Assert(pFont != nullptr, SDL_GetError());
+
+	SDL_Color color;
+	color.r = colour.r;
+	color.g = colour.g;
+	color.b = colour.b;
+	color.a = colour.a;
+
+	SDL_Surface* pSurface{ TTF_RenderText_Blended(pFont, text.c_str(), color) };
+
+	Utils::Assert(pSurface != nullptr, SDL_GetError());
+	TTF_CloseFont(pFont);
 
 	m_Width = pSurface->w;
 	m_Height = pSurface->h;
--- MachineLearning/Texture/Texture.h
+++ MachineLearning/Texture/Texture.h
@@ -1,12 +1,14 @@
 #pragma once
-
 #include <string>
+
+#include "../Math/MathUtils.h"
 
 struct SDL_Texture;
 class Texture final
 {
 public:
 	Texture(const std::string& path);
+	Texture(const std::string& path, const std::string& text, const int ptSize, const MathUtils::RGBColour& colour);
 	~Texture();
 
 	SDL_Texture* const GetSDLTexture() const noexcept;
