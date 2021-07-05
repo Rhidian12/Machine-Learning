cf32894a460c341df00ec9ca3e2deeefb9006322
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-07-05 20:10:05 +0200

    Added a SafeDelete

--- MachineLearning/Utils/Utils.h
+++ MachineLearning/Utils/Utils.h
@@ -10,5 +10,16 @@ namespace Utils
 #else
 	void Assert(const bool expression, std::string&& message);
 #endif
+
 	void DrawCircle(const MathUtils::Point2f& position, const int radius, const MathUtils::RGBColour& colour) noexcept;
+
+	template<typename Type>
+	inline void SafeDelete(Type*& pData)
+	{
+		if (pData)
+		{
+			delete pData;
+			pData = nullptr;
+		}
+	}
 }
