365a294fea82567e5f5eadb0b1ab9d9686e832de
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-07-05 21:25:22 +0200

    Added rule of 5 to Texture

--- MachineLearning/Texture/Texture.cpp
+++ MachineLearning/Texture/Texture.cpp
@@ -36,15 +36,14 @@ Texture::Texture(const std::string& path, const std::string& text, const int ptS
 	Utils::Assert(pFont != nullptr, SDL_GetError());
 
 	SDL_Color color;
-	color.r = colour.r;
-	color.g = colour.g;
-	color.b = colour.b;
-	color.a = colour.a;
+	color.r = Uint8(colour.r);
+	color.g = Uint8(colour.g);
+	color.b = Uint8(colour.b);
+	color.a = Uint8(colour.a);
 
 	SDL_Surface* pSurface{ TTF_RenderText_Blended(pFont, text.c_str(), color) };
 
 	Utils::Assert(pSurface != nullptr, SDL_GetError());
-	TTF_CloseFont(pFont);
 
 	m_Width = pSurface->w;
 	m_Height = pSurface->h;
@@ -53,12 +52,45 @@ Texture::Texture(const std::string& path, const std::string& text, const int ptS
 
 	Utils::Assert(m_pTexture != nullptr, SDL_GetError());
 
+	TTF_CloseFont(pFont);
 	SDL_FreeSurface(pSurface);
 }
 
 Texture::~Texture()
 {
 	SDL_DestroyTexture(m_pTexture);
+}
+
+Texture::Texture(const Texture& other) noexcept
+	: m_pTexture{ other.m_pTexture }
+	, m_Width{ other.m_Width }
+	, m_Height{ other.m_Height }
+{
+}
+
+Texture::Texture(Texture&& other) noexcept
+	: m_pTexture{ std::move(other.m_pTexture) }
+	, m_Width{ std::move(other.m_Width) }
+	, m_Height{ std::move(other.m_Height) }
+{
+	other.m_pTexture = nullptr;
+}
+
+Texture& Texture::operator=(const Texture& other) noexcept
+{
+	m_pTexture = other.m_pTexture;
+	m_Width = other.m_Width;
+	m_Height = other.m_Height;
+	return *this;
+}
+
+Texture& Texture::operator=(Texture&& other) noexcept
+{
+	m_pTexture = std::move(other.m_pTexture);
+	m_Width = std::move(other.m_Width);
+	m_Height = std::move(other.m_Height);
+	other.m_pTexture = nullptr;
+	return *this;
 }
 
 SDL_Texture* const Texture::GetSDLTexture() const noexcept
--- MachineLearning/Texture/Texture.h
+++ MachineLearning/Texture/Texture.h
@@ -11,6 +11,11 @@ public:
 	Texture(const std::string& path, const std::string& text, const int ptSize, const MathUtils::RGBColour& colour);
 	~Texture();
 
+	Texture(const Texture& other) noexcept;
+	Texture(Texture&& other) noexcept;
+	Texture& operator=(const Texture& other) noexcept;
+	Texture& operator=(Texture&& other) noexcept;
+
 	SDL_Texture* const GetSDLTexture() const noexcept;
 	const int GetWidth() const noexcept;
 	const int GetHeight() const noexcept;
