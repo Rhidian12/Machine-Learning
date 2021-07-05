fb308b42edc647d1ef542d2244b5a1b5d81ab3b2
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-07-05 19:53:21 +0200

    finished (?) node

--- MachineLearning/Node/Node.cpp
+++ MachineLearning/Node/Node.cpp
@@ -1,1 +1,14 @@
 #include "Node.h"
+#include "../Utils/Utils.h"
+
+Node::Node(MathUtils::Point2f&& position, MathUtils::RGBColour&& colour, const uint32_t index)
+	: m_Position{ position }
+	, m_Colour{ colour }
+	, m_Index{ index }
+{
+}
+
+void Node::Render() const noexcept
+{
+	Utils::DrawCircle(m_Position, 10, MathUtils::RGBColour{ 192.f, 192.f, 192.f });
+}
--- MachineLearning/Node/Node.h
+++ MachineLearning/Node/Node.h
@@ -1,7 +1,18 @@
 #pragma once
 
+#include "../Math/MathUtils.h"
+
+#include <stdint.h>
+
 class Node final
 {
 public:
+	Node(MathUtils::Point2f&& position, MathUtils::RGBColour&& colour, const uint32_t index);
 
+	void Render() const noexcept;
+
+private:
+	MathUtils::Point2f m_Position;
+	MathUtils::RGBColour m_Colour;
+	uint32_t m_Index;
 };
