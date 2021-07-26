e2040cf1ba4eea469110a0ec00b81121e5dbc162
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-07-26 02:20:37 +0200

    imlemented basic dino

--- MachineLearning/DinoGameML/Dino/Dino.cpp
+++ MachineLearning/DinoGameML/Dino/Dino.cpp
@@ -1,1 +1,21 @@
 #include "Dino.h"
+#include "../../Renderer/Renderer.h"
+
+Dino::Dino(const MathUtils::Point2f position, const float speed)
+	: m_Position{ position }
+	, m_Velocity{ m_Speed, 0.f }
+	, m_Texture{ "../Textures/Dino.png" }
+	, m_Speed{ speed }
+	, m_IsJumping{}
+{
+}
+
+void Dino::Update() noexcept
+{
+	m_Velocity.y -= 9.81f; // Apply gravity
+}
+
+void Dino::Render() const noexcept
+{
+	Renderer::GetInstance()->Render(&m_Texture, m_Position);
+}
--- MachineLearning/DinoGameML/Dino/Dino.h
+++ MachineLearning/DinoGameML/Dino/Dino.h
@@ -1,9 +1,22 @@
 #pragma once
 
+#include "../../Math/MathUtils.h"
+#include "../../Texture/Texture.h"
+
 class Dino final
 {
 public:
+	Dino(const MathUtils::Point2f position, const float speed);
+
+	void Update() noexcept;
+	void Render() const noexcept;
 
 private:
+	MathUtils::Point2f m_Position;
+	MathUtils::Vector2f m_Velocity;
 
+	Texture m_Texture;
+
+	float m_Speed;
+	bool m_IsJumping;
 };
