cb3753a2dd1558df0f7f292e010377eafd445d48
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-07-26 02:58:03 +0200

    made basic Dino scene

--- MachineLearning/DinoGameML/DinoGameML.cpp
+++ MachineLearning/DinoGameML/DinoGameML.cpp
@@ -1,1 +1,19 @@
 #include "DinoGameML.h"
+#include "../Math/MathUtils.h"
+
+DinoGameML::DinoGameML()
+	: m_Ground{ MathUtils::Point2f{0.f, 100.f} }
+	, m_Dino{ MathUtils::Point2f{50.f, 100.f}, 50.f, 150.f, &m_Ground }
+{
+}
+
+void DinoGameML::Update(const float dt) noexcept
+{
+	m_Dino.Update(dt);
+}
+
+void DinoGameML::Render() const noexcept
+{
+	m_Dino.Render();
+	m_Ground.Render();
+}
--- MachineLearning/DinoGameML/DinoGameML.h
+++ MachineLearning/DinoGameML/DinoGameML.h
@@ -1,7 +1,17 @@
 #pragma once
 
+#include "Dino/Dino.h"
+#include "Ground/Ground.h"
+
 class DinoGameML final
 {
 public:
+	DinoGameML();
 
+	void Update(const float dt) noexcept;
+	void Render() const noexcept;
+
+private:
+	Ground m_Ground;
+	Dino m_Dino;
 };
