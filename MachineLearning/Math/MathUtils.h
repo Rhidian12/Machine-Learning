ead7858c4b45bfa18629d21c2216e1221c7da7e9
Rhidian De Wit <74491212+Rhidian12@users.noreply.github.com>
2021-08-16 11:38:28 +0200

    made a random number generator

--- MachineLearning/Math/MathUtils.h
+++ MachineLearning/Math/MathUtils.h
@@ -140,6 +140,20 @@ namespace MathUtils
 		return abs(a - b) <= std::numeric_limits<FundamentalType>::epsilon();
 	}
 
+	template<typename FundamentalType, typename = std::enable_if_t<std::is_fundamental_v<FundamentalType>>>
+	constexpr FundamentalType RandomNumber(const FundamentalType min, const FundamentalType max)
+	{
+		if constexpr (std::is_floating_point_v<FundamentalType>)
+		{
+			// https://stackoverflow.com/questions/686353/random-float-number-generation
+			return min + static_cast<FundamentalType>(rand()) / (static_cast<FundamentalType>(RAND_MAX / (max - min)));
+		}
+		else
+		{
+			return static_cast<FundamentalType>(rand()) % max + min;
+		}
+	}
+
 	template<typename Type>
 	constexpr void Clamp(Type& value, const Type& min, const Type& max) noexcept
 	{
